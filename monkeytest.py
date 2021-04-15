b2fda8909f379cdf933f2102d2fd9575ca5b4b29
wallace <11wallace11@gmail.com>
2021-04-15 05:02:41 +0300

    PEPify code and apply best practices

--- monkeytest.py
+++ monkeytest.py
@@ -13,7 +13,7 @@ and deleted, so the script doesn't waste your drive
 
 from random import shuffle
 from time import perf_counter as time
-import argparse
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 import json
 import os
 import sys
@@ -43,48 +43,49 @@ Eat bananas, drink coffee & enjoy!
 
 
 def get_args():
-    parser = argparse.ArgumentParser(description='Arguments', formatter_class = argparse.ArgumentDefaultsHelpFormatter)
+    common_params = {
+        'required': False,
+        'action': 'store'
+    }
+    parser = ArgumentParser(description='Arguments',
+                            formatter_class=ArgumentDefaultsHelpFormatter)
     parser.add_argument('-f', '--file',
-                        required=False,
-                        action='store',
                         default='/tmp/monkeytest',
-                        help='The file to read/write to')
+                        help='The file to read/write to',
+                        **common_params)
     parser.add_argument('-s', '--size',
-                        required=False,
-                        action='store',
                         type=int,
                         default=128,
-                        help='Total MB to write')
+                        help='Total MB to write',
+                        **common_params)
     parser.add_argument('-w', '--write-block-size',
-                        required=False,
-                        action='store',
                         type=int,
                         default=1024,
-                        help='The block size for writing in bytes')
+                        help='The block size for writing in bytes',
+                        **common_params)
     parser.add_argument('-r', '--read-block-size',
-                        required=False,
-                        action='store',
                         type=int,
                         default=512,
-                        help='The block size for reading in bytes')
+                        help='The block size for reading in bytes',
+                        **common_params)
     parser.add_argument('-j', '--json',
-                        required=False,
-                        action='store',
-                        help='Output to json file')
+                        help='Output to json file',
+                        **common_params)
     args = parser.parse_args()
     return args
 
 
 class Benchmark:
 
-    def __init__(self, file,write_mb, write_block_kb, read_block_b):
+    def __init__(self, file, write_mb, write_block_kb, read_block_b):
         self.file = file
         self.write_mb = write_mb
         self.write_block_kb = write_block_kb
         self.read_block_b = read_block_b
         wr_blocks = int(self.write_mb * 1024 / self.write_block_kb)
         rd_blocks = int(self.write_mb * 1024 * 1024 / self.read_block_b)
-        self.write_results = self.write_test( 1024 * self.write_block_kb, wr_blocks)
+        self.write_results = self.write_test(
+            1024 * self.write_block_kb, wr_blocks)
         self.read_results = self.read_test(self.read_block_b, rd_blocks)
 
     def write_test(self, block_size, blocks_count, show_progress=True):
@@ -93,7 +94,7 @@ class Benchmark:
         of blocks_count, each at size of block_size bytes to disk.
         Function returns a list of write times in sec of each block.
         '''
-        f = os.open(self.file, os.O_CREAT | os.O_WRONLY, 0o777)  # low-level I/O
+        f = os.open(self.file, flags=os.O_CREAT | os.O_WRONLY)  # low-level I/O
 
         took = []
         for i in range(blocks_count):
@@ -119,14 +120,15 @@ class Benchmark:
         bytes until the End Of File reached.
         Returns a list of read times in sec of each block.
         '''
-        f = os.open(self.file, os.O_RDONLY, 0o777)  # low-level I/O
+        f = os.open(self.file, flags=os.O_RDONLY)  # low-level I/O
         # generate random read positions
         offsets = list(range(0, blocks_count * block_size, block_size))
         shuffle(offsets)
 
         took = []
         for i, offset in enumerate(offsets, 1):
-            if show_progress and i % int(self.write_block_kb * 1024 / self.read_block_b) == 0:
+            if show_progress and i % int(
+                    self.write_block_kb * 1024 / self.read_block_b) == 0:
                 # read is faster than write, so try to equalize print period
                 sys.stdout.write('\rReading: {:.2f} %'.format(
                     (i + 1) * 100 / blocks_count))
@@ -135,7 +137,8 @@ class Benchmark:
             os.lseek(f, offset, os.SEEK_SET)  # set position
             buff = os.read(f, block_size)  # read from position
             t = time() - start
-            if not buff: break  # if EOF reached
+            if not buff:
+                break  # if EOF reached
             took.append(t)
 
         os.close(f)
@@ -144,39 +147,47 @@ class Benchmark:
     def print_result(self):
         result = ('\n\nWritten {} MB in {:.4f} s\nWrite speed is  {:.2f} MB/s'
                   '\n  max: {max:.2f}, min: {min:.2f}\n'.format(
-            self.write_mb, sum(self.write_results), self.write_mb / sum(self.write_results),
-            max=self.write_block_kb / (1024 * min(self.write_results)),
-            min=self.write_block_kb / (1024 * max(self.write_results))))
+                      self.write_mb, sum(
+                          self.write_results), self.write_mb / sum(self.write_results),
+                      max=self.write_block_kb /
+                      (1024 * min(self.write_results)),
+                      min=self.write_block_kb / (1024 * max(self.write_results))))
         result += ('\nRead {} x {} B blocks in {:.4f} s\nRead speed is  {:.2f} MB/s'
                    '\n  max: {max:.2f}, min: {min:.2f}\n'.format(
-            len(self.read_results), self.read_block_b,
-            sum(self.read_results), self.write_mb / sum(self.read_results),
-            max=self.read_block_b / (1024 * 1024 * min(self.read_results)),
-            min=self.read_block_b / (1024 * 1024 * max(self.read_results))))
+                       len(self.read_results), self.read_block_b,
+                       sum(self.read_results), self.write_mb /
+                       sum(self.read_results),
+                       max=self.read_block_b /
+                       (1024 * 1024 * min(self.read_results)),
+                       min=self.read_block_b / (1024 * 1024 * max(self.read_results))))
         print(result)
         print(ASCIIART)
 
-
-    def get_json_result(self,output_file):
-        results_json = {}
-        results_json["Written MB"] = self.write_mb
-        results_json["Write time (sec)"] = round(sum(self.write_results),2)
-        results_json["Write speed in MB/s"] = round(self.write_mb / sum(self.write_results),2)
-        results_json["Read blocks"] = len(self.read_results)
-        results_json["Read time (sec)"] = round(sum(self.read_results),2)
-        results_json["Read speed in MB/s"] = round(self.write_mb / sum(self.read_results),2)
-        with open(output_file,'w') as f:
-            json.dump(results_json,f)
+    def get_json_result(self, output_file):
+        results_json = {
+            'Written MB': self.write_mb,
+            'Write time (sec)': round(sum(self.write_results), 2),
+            'Write speed in MB/s': round(self.write_mb / sum(self.write_results), 2),
+            'Read blocks': len(self.read_results),
+            'Read time (sec)': round(sum(self.read_results), 2),
+            'Read speed in MB/s': round(self.write_mb / sum(self.read_results), 2)
+        }
+        with open(output_file, 'w') as f:
+            json.dump(results_json, f)
 
 
 def main():
     args = get_args()
-    benchmark = Benchmark(args.file, args.size, args.write_block_size, args.read_block_size)
+    benchmark = Benchmark(file=args.file,
+                          write_mb=args.size,
+                          write_block_kb=args.write_block_size,
+                          read_block_b=args.read_block_size)
     if args.json is not None:
         benchmark.get_json_result(args.json)
     else:
         benchmark.print_result()
     os.remove(args.file)
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
